d7593ae05682f28a6e42f6ae073ab4c4628444f8
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-09-24 21:19:17 +0900

    ログアウトボタンを押した後の処理を追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,9 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  # サインアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
