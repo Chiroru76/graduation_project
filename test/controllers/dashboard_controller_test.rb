f7c4f22c089e0c019b570c606460ece34a37693f
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-09-27 12:20:37 +0900

    テストで current_user が nil になる問題を解消

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
-  setup do
-    @user = users(:one)
-  end
+
 
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
   test "should get show" do
     sign_in @user
     get dashboard_show_url
