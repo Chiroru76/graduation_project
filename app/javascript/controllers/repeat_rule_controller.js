b8362e80b0b94700c21fb22de91554bfad5fec99
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-09-28 22:22:38 +0900

    習慣作成画面に曜日チェック画面を追加

--- app/javascript/controllers/repeat_rule_controller.js
+++ app/javascript/controllers/repeat_rule_controller.js
@@ -2,6 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="repeat-rule"
 export default class extends Controller {
+  static targets = ["checkbox", "output"]
+
   connect() {
+    this.updateOutput();
   }
+
+  toggle() {
+    this.updateOutput()
+  }
+
+  updateOutput() {
+    const selected = this.checkboxTargets
+      .filter((cb) => cb.checked)
+      .map((cb) => cb.value);
+
+    // hidden に JSON をセット
+    this.hiddenTarget.value = JSON.stringify({ days: days })
+
+    this.outputTarget.textContent =
+      selected.length > 0 ? `選択中: ${selected.join(", ")}` : "未選択";
+  }
+
 }
