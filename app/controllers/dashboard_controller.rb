6b4b3cce04b4c5083ada01445a6c7c9e23bfe1c0
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-09-30 21:59:27 +0900

    タスク一覧をTODO一覧と習慣一覧に分割

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   def show
-    @tasks = current_user.tasks.order(:created_at)
+    @todos = current_user.tasks.todo.order(:created_at)
+    @habits = current_user.tasks.habit.order(:created_at)
   end
 end
