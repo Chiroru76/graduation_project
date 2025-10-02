ea6f299ac5000e6ea372bf313271901b2743b1a4
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-10-03 08:00:44 +0900

    Userモデルにリレーションを追加し「ユーザーが持つキャラ一覧」と「その中で現在育成中のキャラ」を同時に管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tasks, dependent: :destroy
+  has_many :characters
+  belongs_to :character, optional: true
 end
