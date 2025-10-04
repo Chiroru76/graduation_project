f3af9dd54a620264e752ae8351c89a75f735cc9e
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-10-04 17:54:47 +0900

    レベルアップに必要な経験値のしきい値を設定

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,4 +8,11 @@ class Character < ApplicationRecord
   validates :level, numericality: { greater_than_or_equal_to: 1 }
   validates :exp, :bond_hp, :bond_hp_max, numericality: { greater_than_or_equal_to: 0 }
   validates :bond_hp, numericality: { less_than_or_equal_to: :bond_hp_max }
+
+    #レベルアップに必要な経験値の計算式
+  def self.threshold_exp_for_next_level(level)
+    return 0 if level < 1
+    # レベルが上がるごとに値が1.2倍される
+    (100 * (1.2 ** (level - 1))).to_i
+  end
 end
