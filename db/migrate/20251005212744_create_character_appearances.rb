60e8c061e8825d035bbf6685368ffb59bf4f9320
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-10-06 06:36:27 +0900

    マイグレーション実行haracter_appearancesテーブル作成

--- db/migrate/20251005212744_create_character_appearances.rb
+++ db/migrate/20251005212744_create_character_appearances.rb
@@ -2,8 +2,8 @@ class CreateCharacterAppearances < ActiveRecord::Migration[8.0]
   def change
     create_table :character_appearances do |t|
       t.references :character_kind, null: false, foreign_key: true
-      t.integer :pose
-      t.integer :asset_kind
+      t.integer :pose, null: false, default: 0
+      t.integer :asset_kind, null: false, default: 0
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_05_122129) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_05_212744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "character_appearances", force: :cascade do |t|
+    t.bigint "character_kind_id", null: false
+    t.integer "pose", default: 0, null: false
+    t.integer "asset_kind", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["character_kind_id"], name: "index_character_appearances_on_character_kind_id"
+  end
 
   create_table "character_kinds", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +86,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_05_122129) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "character_appearances", "character_kinds"
   add_foreign_key "characters", "character_kinds"
   add_foreign_key "characters", "users"
   add_foreign_key "tasks", "users"
