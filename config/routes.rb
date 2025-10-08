ead0e320d93508051f370f652327b425384ee55a
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-10-08 22:58:34 +0900

    rails g controller Welcomeを実行
    新規登録後にたまごを入手する画面を表示させるために、Welcomeコントローラーとそのビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
     end
   end
   # 新規登録後のたまご入手画面
-  get "welcome/egg", to => "welcome#egg", as: :welcome_egg
-  get "dashboard/show"
+  get "welcome/egg", to: "welcome#egg", as: :welcome_egg
+  get "dashboard", to: "dashboard#show", as: :dashboard_show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -25,5 +25,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "dashboard#show"
+
 end
