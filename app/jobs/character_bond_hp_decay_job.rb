764a6ef721be289b3a471a4ec8031a308dfc754d
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-10-18 16:26:48 +0900

    減衰処理後にbond_hp <= 0をチェックして死亡処理を呼び出し
    - bond_hpの減衰対象をuser.active_characterのみに変更

--- app/jobs/character_bond_hp_decay_job.rb
+++ app/jobs/character_bond_hp_decay_job.rb
@@ -5,8 +5,12 @@ class CharacterBondHpDecayJob < ApplicationJob
     INACTIVE_DECAY_AMOUNT = 20 # 1日以上活動(タスクの実行orえさやり)が無い場合に減少するきずなHP量
 
   def perform(*args)
-    inactive_since = 1.days.ago
-    Character.find_each do |character|
+    inactive_since = 1.day.ago
+
+    User.includes(:active_character).find_each do |user|
+      character = user.active_character
+      next unless character
+
       total_decay = DAILY_DECAY_AMOUNT
 
       if character.last_activity_at && character.last_activity_at <= inactive_since
@@ -15,7 +19,12 @@ class CharacterBondHpDecayJob < ApplicationJob
 
       new_bond_hp = [character.bond_hp - total_decay, 0].max
       character.update!(bond_hp: new_bond_hp)
+
+      # bond_hpが0になった場合の死亡処理
+      if new_bond_hp == 0 && character.alive?
+        character.die!
+      end
     end
-    Rails.logger.info "[CharacterBondHpDecayJob] #{Character.count} characters processed at #{Time.current}"
+    Rails.logger.info "[CharacterBondHpDecayJob] processed at #{Time.current}"
   end
 end
