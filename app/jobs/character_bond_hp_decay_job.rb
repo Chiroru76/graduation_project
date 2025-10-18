4058355867ed2dc87f62d40fe04d1cdce5a673e9
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-10-18 14:49:58 +0900

    毎日00:00にbond_hpを減少させるジョブを追加する
    - 毎日00:00になると、全てのキャラクターのbond_hpを10減少
    - さらに、1日以上活動が無いキャラクターのbond_hpを追加で20減少

--- app/jobs/character_bond_hp_decay_job.rb
+++ app/jobs/character_bond_hp_decay_job.rb
@@ -1,7 +1,21 @@
 class CharacterBondHpDecayJob < ApplicationJob
   queue_as :default
 
+    DAILY_DECAY_AMOUNT = 10 # 毎日減少するきずなHP量
+    INACTIVE_DECAY_AMOUNT = 20 # 1日以上活動(タスクの実行orえさやり)が無い場合に減少するきずなHP量
+
   def perform(*args)
-    # Do something later
+    inactive_since = 1.days.ago
+    Character.find_each do |character|
+      total_decay = DAILY_DECAY_AMOUNT
+
+      if character.last_activity_at && character.last_activity_at <= inactive_since
+        total_decay += INACTIVE_DECAY_AMOUNT
+      end
+
+      new_bond_hp = [character.bond_hp - total_decay, 0].max
+      character.update!(bond_hp: new_bond_hp)
+    end
+    Rails.logger.info "[CharacterBondHpDecayJob] #{Character.count} characters processed at #{Time.current}"
   end
 end
