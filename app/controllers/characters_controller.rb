8b11cdb249b39c8a26cc94da45e780043b87f095
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-10-19 10:20:29 +0900

    新たにたまごを生成し、ユーザーの育成中キャラとして設定するアクション

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -9,4 +9,20 @@ class CharactersController < ApplicationController
       redirect_to dashboard_show_path, alert: "えさをあげられませんでした"
     end
   end
+
+  def reset
+    # マスターデータからたまごのCharacterKindを取得
+    egg_kind = CharacterKind.find_by!(asset_key: "egg")
+    # 新しいたまごを作成
+    ch = current_user.characters.create!(
+        character_kind: egg_kind,
+        state: :alive,
+        last_activity_at: Time.current
+    )
+
+    # ユーザーの現在育成中キャラクターをたまごに変更
+    current_user.update!(active_character: ch)
+
+    redirect_to welcome_egg_path, notice: "キャラクターをリセットしました"
+  end
 end
