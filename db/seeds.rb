91a0aed6a5256e5680cce1b70928a703127c35a3
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-11-02 19:11:41 +0900

    seeds.rbの更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,19 @@
 
 
 character_kinds = [
-  { asset_key: "egg", stage: 0, name: "Egg", appearances: [ :idle ] },
-  { asset_key: "green_robo", stage: 1, name: "Green Robo", appearances: [ :idle ] },
-  { asset_key: "green_monster", stage: 1, name: "Green Monster", appearances: [ :idle ] },
+  { asset_key: "egg", stage: 0, name: "たまご", appearances: [ :idle ] },
+  { asset_key: "green_robo", stage: 1, name: "グリモン", appearances: [ :idle ] },
+  { asset_key: "green_robo", stage: 2, name: "グリモン", appearances: [ :idle ] },
   { asset_key: "hurozapple", stage: 1, name: "フロザップル", appearances: [ :idle ] },
-  { asset_key: "hurozapple", stage: 2, name: "フロザップル", appearances: [ :idle ] }
+  { asset_key: "hurozapple", stage: 2, name: "フロザップル", appearances: [ :idle ] },
+  { asset_key: "dreamowl", stage: 1, name: "オウルン", appearances: [ :idle ] },
+  { asset_key: "dreamowl", stage: 2, name: "オウルン", appearances: [ :idle ] },
+  { asset_key: "lumya", stage: 1, name: "ルミャ", appearances: [ :idle ] },
+  { asset_key: "lumya", stage: 2, name: "ルミャ", appearances: [ :idle ] },
+  { asset_key: "luna", stage: 1, name: "ルーナ", appearances: [ :idle ] },
+  { asset_key: "luna", stage: 2, name: "ルーナ", appearances: [ :idle ] },
+  { asset_key: "frame", stage: 1, name: "フレム", appearances: [ :idle ] },
+  { asset_key: "frame", stage: 2, name: "フレム", appearances: [ :idle ] }
 ]
 
 character_kinds.each do |data|
@@ -22,9 +30,8 @@ character_kinds.each do |data|
   kind = CharacterKind.find_or_create_by!(
     asset_key: data[:asset_key],
     stage: data[:stage]
-  ) do |k|
-    k.name = data[:name]
-  end
+  )
+  kind.update!(name: data[:name])
 
   # --- CharacterAppearance 登録 ---
   data[:appearances].each do |pose|
