03e2548c8cd0e05fbf57e5404927ed760c06b47b
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-12-02 22:24:23 +0900

    /user.rb に Google OAuth 認証からのユーザー検索/作成ロジックを実装。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :tasks, dependent: :destroy
   # 所有しているペット一覧をuser.charactersで参照できる
   has_many :characters, dependent: :destroy
@@ -10,6 +11,21 @@ class User < ApplicationRecord
   belongs_to :active_character, class_name: "Character", foreign_key: "character_id", optional: true
   # ユーザー作成後にペット作成メソッドを呼ぶ
   after_create_commit :create_initial_character
+  #　uidが存在する場合のみ、その一意性をproviderのスコープ内で確認
+  validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+
 
   private
   def create_initial_character
