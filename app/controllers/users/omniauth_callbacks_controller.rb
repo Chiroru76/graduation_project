4bc5d1ba3eb03df276032773b749c119ad57f52b
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-12-05 12:18:18 +0900

    googleログイン後のリダイレクト先をダッシュボードに変更

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,10 +9,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in @user, event: :authentication
       set_flash_message(:notice, :success, kind: provider.to_s.capitalize) if is_navigational_format?
+      redirect_to dashboard_show_path
     else
-
       flash[:alert] = @user.errors.full_messages.to_sentence if @user.errors.any?
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except(:extra)
       redirect_to new_user_registration_url
