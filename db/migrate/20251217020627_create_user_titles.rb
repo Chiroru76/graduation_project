8a631f00af5b139fe87ae0ae1bcacbc9edaa403b
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-12-17 11:14:11 +0900

    usertitlesテーブルのマイグレーションファイルを作成

---
+++ db/migrate/20251217020627_create_user_titles.rb
@@ -0,0 +1,13 @@
+class CreateUserTitles < ActiveRecord::Migration[8.0]
+  def change
+    create_table :user_titles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :title, null: false, foreign_key: true
+      t.datetime :unlocked_at
+
+      t.timestamps
+    end
+    # 複合ユニークインデックスで 重複付与防止
+    add_index :user_titles, [:user_id, :title_id], unique: true
+  end
+end
