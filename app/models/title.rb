b27d3757578b5cf7326d8da3db3736ffccb881c2
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-12-17 11:05:25 +0900

    Titleモデルのアソシエーションとバリデーションを追加

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,9 @@
 class Title < ApplicationRecord
+  has_many :user_titles, dependent: :destroy
+  has_many :users, through: :user_titles
+
+  validates :key, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :rule_type, presence: true
+  validates :threshold, presence: true, numericality: { greater_than: 0 }
 end
