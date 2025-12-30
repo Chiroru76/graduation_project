eb24f1c334a894a60c09f6c1850bc9a799cf4eb2
Chiroru76 <s.chiharu7.s7@gmail.com>
2025-12-30 12:02:50 +0900

    Protect development DB from DatabaseCleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,23 +1,35 @@
 require 'database_cleaner/active_record'
 
-RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
+# test環境でのみDatabaseCleanerを有効化
+# development/production環境でのデータ削除を防ぐ
+if Rails.env.test?
+  RSpec.configure do |config|
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:truncation)
+    end
 
-  config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
-  end
+    config.before(:each) do
+      DatabaseCleaner.strategy = :transaction
+    end
 
-  config.before(:each, type: :system) do
-    DatabaseCleaner.strategy = :truncation
-  end
+    config.before(:each, type: :system) do
+      DatabaseCleaner.strategy = :truncation
+    end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+  end
+else
+  # test環境以外で実行された場合は警告を表示
+  RSpec.configure do |config|
+    config.before(:suite) do
+      puts "\n⚠️  WARNING: RSpec is running in #{Rails.env} environment!"
+      puts "   DatabaseCleaner is DISABLED to protect your data.\n\n"
+    end
   end
 end
